d898708271b24391b192cb6cb89b25e85dbd48ab
Aqila Ghifari Wandana <ghifariwandana@gmail.com>
2024-09-26 21:04:03 +0700

    routing fixing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // Membersihkan cache setiap hari
+        $schedule->command('cache:clear')->daily();
     }
+
 
     /**
      * Register the commands for the application.
@@ -25,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
